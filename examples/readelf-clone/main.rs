6ad7e64141e90e08e217e0d0214c8e7945be2516
Hugo C <hugo_cebrecos_s@protonmail.com>
2026-04-16 20:35:59 +0200

    feat: added readelf-like example

---
+++ examples/readelf-clone/main.rs
@@ -0,0 +1,92 @@
+use std::{
+    env,
+    fs::File,
+    io::{Read, Seek, SeekFrom},
+};
+
+use elftool::{
+    elf::{ElfErr, ElfReader, header::*},
+    reader::ReaderCtx,
+};
+
+#[derive(Debug)]
+pub struct FileReader {
+    file: File,
+}
+
+impl FileReader {
+    pub fn open(path: &str) -> Result<Self, ElfErr> {
+        let file = File::open(path).map_err(|_| ElfErr::IoError)?;
+        Ok(Self { file })
+    }
+}
+
+impl ElfReader for FileReader {
+    fn read(&mut self, offset: u64, buf: &mut [u8]) -> Result<(), ElfErr> {
+        self.file
+            .seek(SeekFrom::Start(offset))
+            .map_err(|_| ElfErr::IoError)?;
+
+        self.file.read_exact(buf).map_err(|_| ElfErr::IoError)?;
+
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), ElfErr> {
+    let path = env::args().nth(1).expect("usage: readelf_rs <file>");
+
+    let reader = FileReader::open(&path)?;
+    let ctx = ReaderCtx::new(reader)?;
+    let hdr = ctx.get_hdr();
+
+    println!("ELF Header:");
+    println!(
+        "  Class:                             {}",
+        ELFCLASS::to_str(hdr.ei_class)
+    );
+    println!(
+        "  Data:                              {}",
+        ELFDATA::to_str(hdr.ei_data)
+    );
+    println!(
+        "  Version:                           {} {}",
+        hdr.version,
+        EV::to_str(hdr.version)
+    );
+    println!("  OS/ABI:                            {}", ELFOSABI::to_str(hdr.ei_os_abi));
+    println!("  ABI Version:                       {}", hdr.ei_abi_ver);
+    println!(
+        "  Type:                              {}",
+        ET::to_str(hdr.elf_type)
+    );
+    println!("  Machine:                           {:?}", hdr.machine); //TODO
+    println!("  Version:                           0x{:x}", hdr.version);
+    println!("  Entry point address:               0x{:x}", hdr.entry);
+    println!(
+        "  Start of program headers:          {} (bytes into file)",
+        hdr.pro_hdr_off
+    );
+    println!(
+        "  Start of section headers:          {} (bytes into file)",
+        hdr.sec_hdr_off
+    );
+    println!("  Flags:                             0x{:x}", hdr.flags);
+    println!(
+        "  Size of this header:               {} (bytes)",
+        hdr.hdr_size
+    );
+    println!(
+        "  Size of program headers:           {} (bytes)",
+        hdr.ph_entry_sz
+    );
+    println!("  Number of program headers:         {}", hdr.ph_entry_num);
+    println!(
+        "  Size of section headers:           {} (bytes)",
+        hdr.sh_entry_sz
+    );
+    println!("  Number of section headers:         {}", hdr.sh_entry_num);
+    println!("  Section header string table index: {}", hdr.sec_str_idx);
+
+    Ok(())
+}
